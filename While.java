b9a6640ee0c9da26de746f57e1770dc50f6937ad
unknown <beto.cajueiro@gmail.com>
2021-03-20 22:31:24 -0300

    While add codigo

--- While.java
+++ While.java
@@ -4,6 +4,10 @@ public class While {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int i = 0;
+		while(i<5) {
+			System.out.println("i = " + i++);
+		}
 
 	}
 
