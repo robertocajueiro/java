762c31bde4b6be9ab1eb3c0e2e4b4e3398f5f07d
unknown <beto.cajueiro@gmail.com>
2021-03-20 22:23:11 -0300

    Commit Estudo de Java

--- Fluxo.java
+++ Fluxo.java
@@ -4,6 +4,17 @@ public class Fluxo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		int ano = 0;
+		ano = 2020;
+		
+		if (ano == 2021) {
+			System.out.println("O Ano é 2021");
+		}else {
+			System.out.println("O ano não é 2021");
+		}
+				
+		
 
 	}
 
