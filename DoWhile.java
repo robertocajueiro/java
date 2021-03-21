e75e2797c8c4029e598cf47dd2fd75393aa75c8c
unknown <beto.cajueiro@gmail.com>
2021-03-20 22:37:47 -0300

    add cod DoWhile

--- DoWhile.java
+++ DoWhile.java
@@ -4,7 +4,13 @@ public class DoWhile {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int i = 10;
+		do {
+			System.out.println("i = " + i++);
+		}while(i<5);
+		
 
 	}
 
 }
+//Se a variavel i for 10 ele executara uma unica vez
