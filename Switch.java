3e31f8ced53b206dd6bbd5ce935b4bbb0e53b646
unknown <beto.cajueiro@gmail.com>
2021-03-21 21:12:56 -0300

    Estudo java Switch

--- Switch.java
+++ Switch.java
@@ -20,14 +20,19 @@ public class Switch {
 			break;
 		case 5:
 			System.out.println("Maio");
+			break;
 		case 6:
 			System.out.println("Junho");
+			break;
 		case 7:
 			System.out.println("Julho");
+			break;
 		case 8:
 			System.out.println("Agosto");
+			break;
 		case 9:
 			System.out.println("Setembro");
+			break;
 		case 10:
 			System.out.println("Outubro");
 			break;
