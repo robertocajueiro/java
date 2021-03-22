eaa6d6b6d54a62436ca88d2dd5863cde9c3a3a69
unknown <beto.cajueiro@gmail.com>
2021-03-21 22:12:28 -0300

    Estudo de Java Array

---
+++ Array.java
@@ -0,0 +1,21 @@
+package io.github.robertocajueiro.estudojava2;
+
+public class Array {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char[] nota = new char[5];
+		nota[0] = 'A';
+		nota[1] = 'B';
+		nota[2] = 'C';
+		nota[3] = 'D';
+		nota[4] = 'E';
+		
+		System.out.println(nota[0]);
+		System.out.println(nota[1]);
+		System.out.println(nota[2]);
+		System.out.println(nota[3]);
+		System.out.println(nota[4]);
+	}
+
+}
