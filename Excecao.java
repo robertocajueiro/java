0a32da6ac369f45747430d87c90aed930fbad68c
unknown <beto.cajueiro@gmail.com>
2021-03-21 21:22:55 -0300

    Estudo de Java Excecao

---
+++ Excecao.java
@@ -0,0 +1,20 @@
+package io.github.robertocajueiro.estudojava2;
+
+public class Excecao {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i = 5;
+		int x = 0;
+		int r = 0;
+		
+		
+		try {
+			r = i/x;
+		}catch(Exception e) {
+			System.out.println("r = " + r);
+		}
+
+	}
+
+}
