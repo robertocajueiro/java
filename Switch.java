9e9954e6f83965ee326c3d2fc9a5e60ba6697d44
unknown <beto.cajueiro@gmail.com>
2021-03-21 21:10:58 -0300

    Estudo de Java Switch

---
+++ Switch.java
@@ -0,0 +1,47 @@
+package io.github.robertocajueiro.estudojava2;
+
+public class Switch {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int mes = 10;
+		switch(mes) {
+		case 1:
+			System.out.println("Janeiro");
+			break;
+		case 2:
+			System.out.println("Fevereiro");
+			break;
+		case 3:
+			System.out.println("Março");
+			break;
+		case 4:
+			System.out.println("Abril");
+			break;
+		case 5:
+			System.out.println("Maio");
+		case 6:
+			System.out.println("Junho");
+		case 7:
+			System.out.println("Julho");
+		case 8:
+			System.out.println("Agosto");
+		case 9:
+			System.out.println("Setembro");
+		case 10:
+			System.out.println("Outubro");
+			break;
+		case 11:
+			System.out.println("Novembro");
+		case 12:
+			System.out.println("Dezembro");
+			
+		default:
+			System.out.println("Mês não existe!");
+			
+		}
+		
+
+	}
+
+}
